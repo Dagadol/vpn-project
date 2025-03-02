ce3f68f51d5b85e3dd70a643b933e31156fa3125
JJ <orit@_._>
2025-03-02 23:18:10 +0200

    main server added, updated settings to be programmatically. push.

---
+++ main_server.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+import connect_protocol
+
+this_ip = "10.0.0.20"
+client_port = 5500
+port_for_vpn = 8888
+vpn_servers = dict()  # skt: addr
+
+
+def handle_client(skt, addr):
+    while True:
+        cmd, msg = connect_protocol.get_msg(skt)
+
+
+def listen_for_servers():
+    pass
+
+
+def listen_for_clients():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((this_ip, client_port))
+
+    server_socket.listen(5)
+    threads = []
+    while True:
+        client_socket, addr = server_socket.accept()
+        t = threading.Thread(target=handle_client, args=[client_socket, addr])
+        t.start()
+        threads.append(t)
+
+
+if __name__ == '__main__':
+    main()
