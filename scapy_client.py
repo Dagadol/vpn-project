a262d6ebc0ff168e0d8bac2d33b9395d4fcdf6e2
JJ <orit@_._>
2025-03-11 00:53:29 +0200

    added AES encryptions, with key exchange

--- scapy_client.py
+++ scapy_client.py
@@ -2,6 +2,8 @@ import socket
 import hashlib
 import threading
 from scapy.all import sniff, send
+
+import connect_protocol
 import nat_class
 from scapy.layers.inet import IP
 from vpn_client import connected
@@ -19,37 +21,64 @@ from vpn_client import connected
 # private_ip = "10.0.0.11"
 
 
-def first_connection() -> str:
+def first_connection() -> bytes | None:
     """
+    get shared key, and set the correct port of the vpn server
+    :return: AES key from the server or None in case of an error
+    """
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        try:
+            my_socket.connect((connected[1].vpn_ip, connected[1].vpn_port))
+            break
+        except Exception as e:
+            print("error at first connection\n", e)
 
-    :return: AES key from the server
-    """
-    pass
+    shared_key = connect_protocol.dh_get(my_socket)
+
+    # receive the correct vpn_port
+    cmd, data = connect_protocol.get_msg(my_socket, True)
+
+    my_socket.close()  # close connection
+    if cmd != "f_conn":
+        print("error at first connection:", cmd, data)
+        return None
+
+    # set the port
+    udp_port = connect_protocol.decrypt(data, shared_key)
+    connected[1].vpn_port = udp_port
+
+    return shared_key  # return the key
 
 
 def start_connection():
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind((connected[1].private_ip, connected[1].my_port))
     print("connecting to server")
+    key = first_connection()
+    if key:
 
-    t1 = threading.Thread(target=receive_from_adapter, args=[udp_socket])
-    t2 = threading.Thread(target=receive_from_vpn, args=[udp_socket])
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_socket.bind((connected[1].private_ip, connected[1].my_port))
 
-    # close connection
-    udp_socket.close()
-    print("udp socket was closed")
+        t1 = threading.Thread(target=receive_from_adapter, args=[udp_socket, key])
+        t2 = threading.Thread(target=receive_from_vpn, args=[udp_socket, key])
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+
+        # close connection
+        udp_socket.close()
+        print("udp socket was closed")
 
 
-def send_to_vpn(data, sock):
+def send_to_vpn(data, sock, key):
     # encrypt the data first
-    data = data
-    this_checksum = hashlib.md5(str(bytes(data)).encode()).hexdigest()
+    data = bytes(data)
+    data = connect_protocol.encrypt(data, key)
+
+    this_checksum = hashlib.md5(data).hexdigest()
     print(this_checksum, "data:", data)
-    sock.sendto(this_checksum.encode() + b"~~" + bytes(data), (connected[1].vpn_ip, connected[1].vpn_port))
+    sock.sendto(this_checksum.encode() + b"~~" + data, (connected[1].vpn_ip, connected[1].vpn_port))
 
 
 def scapy_filter(p):
@@ -74,22 +103,26 @@ def active_thread():
     return True
 
 
-def receive_from_adapter(s):
+def receive_from_adapter(s, k):
     """
     sniff of the virtual interface, send over to server via Ethernet interface
+    :param k: key
     :param s: UDP socket
     :return:
     """
-    sniff(prn=lambda p: send_to_vpn(p, s), lfilter=scapy_filter, iface=connected[0].name(), stop_filter=active_thread)
+    sniff(prn=lambda p: send_to_vpn(p, s, k), lfilter=scapy_filter, iface=connected[0].name(),
+          stop_filter=active_thread)
     print("closing confirmed from sniff")
 
 
-def receive_from_vpn(sock):
+def receive_from_vpn(sock, key):
     while not active_thread():
         data, a = sock.recvfrom(65535)
         print("received")
         if a[0] == connected[1].vpn_ip:
             # decrypt data
+            data = connect_protocol.decrypt(data, key)
+
             data = IP(data)
             print("vpn data:", data)
 
