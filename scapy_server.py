312d753a098eda69075947a6fbd4945edaf52552
JJ <orit@_._>
2025-03-11 00:57:38 +0200

    added `vpn_server.` before values

--- scapy_server.py
+++ scapy_server.py
@@ -4,20 +4,23 @@ from scapy.all import sniff, send
 from scapy.layers.inet import IP
 import hashlib
 import threading
+import vpn_server
 
+nat = nat_class.ClassNAT(my_ip="10.0.0.20", users=vpn_server.clients)
 clients = {"10.0.0.50": ("10.0.0.11", 8800)}  # virtual adapter: (skt.ip, skt.port)
-nat = nat_class.ClassNAT(my_ip="10.0.0.20", users=clients)
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket.bind(("0.0.0.0", udp_port))
 
 
 def add_user(addr):
-    nat.users_addr[addr[0]] = addr[1]
+    if addr[0] not in nat.users_addr:
+        nat.users_addr[addr[0]] = addr[1]
 
 
 def main():
-    t_recv = threading.Thread(target=internet_recv, args=[socket], daemon=True)
-    t_send = threading.Thread(target=internet_send, args=[socket], daemon=True)
+    skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    skt.bind(("0.0.0.0", vpn_server.udp_port))
+
+    t_recv = threading.Thread(target=internet_recv, args=[skt], daemon=True)
+    t_send = threading.Thread(target=internet_send, args=[skt], daemon=True)
     t_recv.start()
     t_send.start()
     t_recv.join()
@@ -41,6 +44,8 @@ def valid(data: bytes) -> bytes | bool:
 def internet_send(skt):
     while True:
         data, addr = skt.recvfrom(65535)  # receive from client through udp socket
+        if addr[0] not in vpn_server.keys:
+            continue
         pkt = valid(data)
         if pkt:
             new_pkt = nat.udp_recv(pkt, addr)
