8863eeae69b939ad4f0a3e746412e7ee9050f50d
JJ <orit@_._>
2025-03-20 17:31:53 +0200

    added tcp connection and main to set up it locally

--- scapy_server.py
+++ scapy_server.py
@@ -8,7 +8,7 @@ import threading
 
 
 class OpenServer:
-    def __init__(self, server_ip, clients: dict = None, keys: dict = None):
+    def __init__(self, server_ip, server_port, clients: dict = None, keys: dict = None):
         # default_clients = {"10.0.0.50": ("10.0.0.11", 8800)}  # virtual adapter: (skt.ip, skt.port)
         self.clients = clients  # v_addr: (user_ip, user_port)
 
@@ -24,17 +24,18 @@ class OpenServer:
 
         self.t_recv = threading.Thread(target=self.internet_recv, daemon=True)
         self.t_send = threading.Thread(target=self.internet_send, daemon=True)
+        self.udp_port = server_port
 
         self.conn = False
 
-    def open_conn(self, udp_port):
+    def open_conn(self):
         if not (self.t_recv.is_alive() and self.t_send.is_alive()) or not self.conn:
             self.conn = True
             # self.clients = clients
             # self.nat.users_addr = self.clients
 
             self.skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.skt.bind(("0.0.0.0", udp_port))
+            self.skt.bind(("0.0.0.0", self.udp_port))
 
             self.t_recv.start()
             self.t_send.start()
@@ -45,6 +46,7 @@ class OpenServer:
             self.t_recv.join()
             self.t_send.join()
             self.skt = None
+            print("connection has closed")
 
     def remove_client(self, v_addr):
         del self.nat.users_addr[v_addr]
@@ -111,3 +113,64 @@ class OpenServer:
         while self.conn:
             sniff(prn=lambda p: self.forward_to_client(p), filter="ip", stop_filter=(not self.conn))
             print("sniffed stopped")
+
+
+def tcp_connection(client_ip, this_port, vpn: OpenServer):
+    """
+    the first connection between the client and this server, represented in tcp
+    exchanging keys, and ports.
+    :param vpn: info holder, server. type OpenServer
+    :param this_port: tcp_port
+    :param client_ip: client's private IP s
+    """
+    with threading.Lock:
+        this_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        this_sock.bind(("0.0.0.0", this_port))  # Bind to all interfaces on `this_port`
+        this_sock.listen(1)  # Listen with a backlog of 1 (see below)
+
+        while True:
+            conn, addr = this_sock.accept()
+            print("Connection from", addr)
+            if addr[0] != client_ip:
+                print("Unexpected IP, closing connection.")
+                conn.close()
+                continue
+            # Process the connection from the expected IP
+            # apply diffie-helman protocol
+            shared_key = connect_protocol.dh_send(conn)  # get the shared key
+
+            vpn.keys[client_ip] = shared_key  # save the key
+
+            # send over the udp port to the client of this VPN server
+            data = connect_protocol.create_msg(str(vpn.udp_port), "f_conn", shared_key)  # f stands for first
+            conn.send(data)
+
+            if not vpn.conn:  # activate the thread of vpn if it is not already activated
+                vpn.open_conn()
+                print("connection opened")
+            else:
+                print("does not need to activate")
+
+            conn.close()  # close the temp connection
+            break  # break the loop
+        this_sock.close()
+
+
+if __name__ == '__main__':
+    server = OpenServer(
+        server_ip="10.0.0.22",
+        server_port=8888
+    )
+    tcp_connection(
+        client_ip="10.0.0.12",
+        this_port=5123,
+        vpn=server
+    )
+
+    try:
+        server.open_conn()
+        # Keep main thread alive while connection is active
+        while server.conn:
+            threading.Event().wait(1)
+    except KeyboardInterrupt:
+        server.close_conn()
