3bb3d95cde885eda0fd7afa1e9ece1ed71d8bc99
JJ <orit@_._>
2025-05-21 01:40:36 +0300

    test this

--- nat_class.py
+++ nat_class.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 
 from scapy.layers.inet import IP, UDP, TCP
 import time
@@ -22,8 +22,13 @@ class ClassNAT:
         # maybe less ports per user or more ports, or limit user port use. D
         users_amount *= 500
         users_amount += 50000
-        self.port_pool = deque(range(50000, users_amount))  # might want to validate that all these ports are available
-        self.nat_table = []  # format: ((client.src, client.sport), public port, (client.dst, client.dport))
+        max_ports = 65535 - 50000
+        users_amount = min(users_amount, max_ports // 500)
+        self.port_pool = deque(range(50000, 50000 + users_amount * 500))
+
+        self.nat_map = {}  # (client_ip, client_port, proto): {(dst_ip, dst_port), public_port}
+        self.rev_map = defaultdict(dict)  # {(public_port, proto): (dst_ip, dst_port), (client_ip, client_port)}
+
         self.nat_timeouts = {}  # Track last activity time
         self.cleanup = False
 
@@ -62,45 +67,34 @@ class ClassNAT:
                 print("spoof attack, from:", addr)
                 return None
             try:
-                packet_layer4 = self.tcp_udp(packet_data)
-                if not packet_layer4:
+                packet_layer4, proto = self.tcp_udp(packet_data)
+                if not proto:
                     return None
-                info_address = (packet_source, packet_layer4.sport)
+                info_address = (packet_source, packet_layer4.sport, proto)
             except AttributeError:
                 return None
 
-            dict_table = {sublist[0]: i for i, sublist in enumerate(self.nat_table)}
-            # dict_table format: `(client_vm.src, client.sport): index`
-            # index - index of key in the `nat_table`.
-            # (client.src, client.sport) is addr
-            if info_address not in dict_table:
-                # fixme, only check if info_address match; need to check if dst match too, and update nat_table if not
-                # set port timeout
-                self.nat_timeouts[self.port_pool[0]] = time.time()
-
-                # append source info address, attach a unique public port, and add destination info
-                self.nat_table.append(
-                    (info_address,
-                     self.port_pool.popleft(),
-                     (packet_data.dst, packet_layer4.dport))
-                )
-                index = len(self.nat_table) - 1
+            # Simplify port assignment (remove the for-loop):
+            if info_address not in self.nat_map:
+                # try assign a used port before pulling new one
+                info_dest = (packet_data.dst, packet_layer4.dport)
+                public_port = self.port_pool.popleft()  # Always take a new port
+                self.nat_map[info_address] = (info_dest, public_port)
+                self.rev_map[(public_port, proto)][info_dest] = info_address[:-1]
             else:
                 # print("*****"*10, "reusing connection")
-                index = dict_table[info_address]
+                public_port = self.nat_map[info_address][1]
+                if len(self.nat_map[info_address]) > 2:
+                    print("bad")
 
-                # update port timeout
-                self.nat_timeouts[self.nat_table[index][1]] = time.time()
+            # update port timeout
+            self.nat_timeouts[(info_address, public_port)] = time.time()
 
             # update sources
             packet_data.src = self.vpn_private_ip
-            packet_layer4.sport = self.nat_table[index][1]  # get the public port
+            packet_layer4.sport = public_port  # get the public port
 
             # update checksum
-            # data = update_checksum(bytes(packet_data))
-            """del packet_data.chksum
-            del tcp_udp(packet_data).chksum
-            data = bytes(packet_data)"""
             del packet_layer4.chksum
             del packet_data.chksum
 
@@ -123,63 +117,38 @@ class ClassNAT:
             return None, None
 
         # get layer 4 of the packet  (UDP/TCP)
-        layer4 = self.tcp_udp(scapy_packet)
-        if not layer4:
+        layer4, proto = self.tcp_udp(scapy_packet)
+        if not proto:
             print("invalid internet packet struct:", scapy_packet)
             return None, None
 
         # vpn public port
         public_port = layer4.dport
         # internet source info
-        source_ip = scapy_packet.src
-        source_port = layer4.sport
+        source_ip = scapy_packet.src  # (client_dest_ip)
+        source_port = layer4.sport  # (client_dest_port)
+        info_address = (source_ip, source_port)  # (client_dest_addr)
 
-        dict_table = {sublist[1]: i for i, sublist in enumerate(self.nat_table)}
-        if public_port in dict_table:
-            # update port timeout
-            self.nat_timeouts[public_port] = time.time()
+        if (public_port, proto) in self.rev_map.keys():
+            if info_address in self.rev_map[(public_port, proto)]:
+                # client address info
+                addr = self.rev_map[(public_port, proto)][info_address]
 
-            index = dict_table[public_port]
+                # update port timeout
+                self.nat_timeouts[((addr[0], addr[1], proto), public_port)] = time.time()
 
-            if self.nat_table[index][2] != (source_ip, source_port):
-                # while it is true that dest port may match the public port, letting access the client without question
-                # might be dangerous and wrong. if this packet is legit, it means that the cleanup has cleaned up the
-                # port before the stream ended, and a solution for this might be needed.
-                # 1) letting the timeout time be larger than what it is now (60 seconds)
-                # 2) saving the ports that were lastly cleaned up and for more 60 seconds, if they get here, then they
-                # come back alive (to the nat_table) else they will be permanently deleted.
-                # 3) do solution (1) and raise the amount of the public ports, in port pool. the question is to how much
+                # update destinations to client address
+                layer4.dport = addr[1]  # client's original port
+                scapy_packet.dst = addr[0]
 
-                print("need to update nat_table; unmatch connection:", scapy_packet)
-                return None, None
-
-            # client address info
-            addr = self.nat_table[index][0]
-
-            # ip_header = data[IP]
-
-            # update destinations to client address
-            # ip_header.dst = addr[0]  # client VM's IP address
-            layer4.dport = addr[1]  # client's original port
-
-            scapy_packet.dst = addr[0]
-            # tcp_udp(data).dport = addr[1]
-            # data = data[IP]
-
-            # might need to write a new packet instead of updating existing packet `data`
-            """
-            # try minimize the use of scapy, in order to achieve better performance
-            """
-
-            # update checksum
-            # scapy_packet = IP(update_checksum(bytes(scapy_packet)))
-            del scapy_packet.chksum
-            del layer4.chksum
-            # data contains not enough info in order to
-            # extract variables to match `sendto(data, (-, -))`
-            # it is missing ip and port.
-            # in order to get these, you can use the function `get_socket_dst` in the server code
-            return scapy_packet, self.get_socket_dst(ip=addr[0])
+                # update checksum
+                del scapy_packet.chksum
+                del layer4.chksum
+                # data contains not enough info in order to
+                # extract variables to match `sendto(data, (-, -))`
+                # it is missing ip and port.
+                # in order to get these, you can use the function `get_socket_dst` in the server code
+                return scapy_packet, self.get_socket_dst(ip=addr[0])
 
         # print(f"invalid connection: {data}")
         return None, None
@@ -189,16 +158,45 @@ class ClassNAT:
         while self.cleanup:
             time.sleep(30)
             now = time.time()
-            stale = [port for port, t in self.nat_timeouts.items() if now - t > 90]
-            for port in stale:
-                del self.nat_timeouts[port]
-                self.port_pool.append(port)
-                self.nat_table = [i for i in self.nat_table if i[1] != port]
+
+            ports_to_recycle = list(set([(info_addr[2], port) for (info_addr, port) in self.nat_timeouts.keys()]))
+            amount_of_active_ports = len(ports_to_recycle)
+
+            stale = deque()
+            for (info_addr, port), t in self.nat_timeouts.items():
+                if info_addr[2] == "tcp":
+                    if now - t > 120:
+                        stale.append((info_addr, port))
+                    else:
+                        if ("tcp", port) in ports_to_recycle:
+                            ports_to_recycle.remove(("tcp", port))
+
+                else:  # udp case
+                    if now - t >= 30:
+                        stale.append((info_addr, port))
+                    else:
+                        if ("udp", port) in ports_to_recycle:
+                            ports_to_recycle.remove(("udp", port))
+
+            for proto, port in ports_to_recycle:
+                # Remove all rev_map entries for (port, proto)
+                del self.rev_map[(port, proto)]
+                # Return the port to the pool if not already there
+                if port not in self.port_pool:
+                    self.port_pool.append(port)
+
+            for info_addr, port in stale:
+                del self.nat_timeouts[(info_addr, port)]
+                del self.nat_map[info_addr]
+                self.rev_map = defaultdict(dict, {k: {sub_k: sub_v for sub_k, sub_v in v.items() if
+                                                      sub_v != info_addr[:-1] or k != (port, info_addr[2])} for k, v in
+                                                  self.rev_map.items()})
 
             cleaned_up = len(stale)
+            amount_of_active_ports -= len(ports_to_recycle)
             if cleaned_up:
                 print(f"Cleanup: Removed {cleaned_up} entries")
-            print(f"entries amount: {len(self.nat_table)}")
+            print(f"connections amount: {len(self.nat_map)}, Entries amount: {amount_of_active_ports}")
 
     @staticmethod
     def tcp_udp(p):
@@ -209,15 +207,39 @@ class ClassNAT:
         """
         if IP in p:
             if UDP in p:
-                return p[UDP]
+                return p[UDP], "udp"
             elif TCP in p:
-                return p[TCP]
+                return p[TCP], "tcp"
         print("problems with the transport layer of the packet")
         try:
-            return p[UDP]
+            return p[UDP], "udp"
         except IndexError:
             try:
-                return p[TCP]
+                return p[TCP], "tcp"
             except IndexError:
                 print("problematic packet is:", p)
-                return None
+                return None, None
+
+
+def temp_save(self=None, info_address=None, packet_data=None, packet_layer4=None, proto=None):
+    if info_address not in self.nat_map.keys():
+        # try assign a used port before pulling new one
+        info_dest = (packet_data.dst, packet_layer4.dport)
+        public_port = 0
+        for (port, temp_proto) in self.rev_map.keys():
+            if temp_proto != proto:
+                if (port, proto) not in self.rev_map.keys():
+                    public_port = port
+                    break
+            else:
+                if info_dest not in self.rev_map[(port, temp_proto)]:
+                    public_port = port
+                    break
+
+        # pull a new port
+        if not public_port:
+            public_port = self.port_pool.popleft()
+
+        # append source info address, attach a unique public port, and add destination info
+        self.nat_map[info_address] = (info_dest, public_port)
+        self.rev_map[(public_port, proto)][info_dest] = info_address[:-1]
